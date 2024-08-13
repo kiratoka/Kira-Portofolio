3247bc61e6a62152adebef59abe69d1e0937f69b
unknown <kiratoka2@gmail.com>
2024-08-13 22:02:54 +0700

    penghapusan skills

--- data/index.ts
+++ data/index.ts
@@ -4,7 +4,6 @@ export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
   { name: "Testimonials", link: "#testimonials" },
-  { name: "Skills", link: "#skills" },
   { name: "Contact", link: "#contact" },
 ];
 
